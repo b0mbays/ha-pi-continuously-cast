65bd4b270d01767cfd787cfa1911c516643d9165
b0mbays <55556007+b0mbays@users.noreply.github.com>
2023-03-20 14:18:38 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ pushd /opt/ha-cast
 echo "Installing pip3.."
 apt-get -y install python3 python3-pip
 
-echo "***Installing requirements..."
+echo "Installing requirements..."
 pip3 install -r /opt/ha-cast/requirements.txt
 
 echo "Creating a new user for the service.."
